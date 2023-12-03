34b85fd1de34392f01153a4b800a5f1dd23fc0f5
Isaiah Rosinski <irosinsk@pnw.edu>
2023-12-03 14:05:05 -0600

    Add login session functionality

--- src/webapp/index.js
+++ src/webapp/index.js
@@ -1,18 +1,35 @@
 "use strict";
 const express = require("express");
 const path = require("path");
-const ex = express();
+const cookieSession = require("cookie-session");
+const app = express();
 const port = 8080;
 
-ex.get("/", function(req, res) {
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(cookieSession({
+    name: "session",
+    keys: ["secret-key"],
+    sameSite: true
+}));
+
+
+app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "html", "index.html"));
 });
-ex.get("/login", function (req, res) {
+
+app.get("/login", function (req, res) {
+    console.log(req.session);
     res.sendFile(path.join(__dirname, "html", "login.html"));
 });
-ex.get("/manager", function (req, res) {
+app.post("/login", function (req, res) {
+    req.session.ssn = req.body.ssn;
+    res.sendFile(path.join(__dirname, "html", "login.html"));
+});
+
+app.get("/manager", function (req, res) {
     res.sendFile(path.join(__dirname, "html", "manager.html"));
 });
-ex.listen(port, function(){
+app.listen(port, function(){
     console.log(`Listening on ${port}`);
 });
