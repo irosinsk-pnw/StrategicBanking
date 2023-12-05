4c5b43594c0c9f884d7358cb7a4cdfbf99cf9106
Isaiah Rosinski <irosinsk@pnw.edu>
2023-12-04 23:20:02 -0600

    Finish implementing all the things.
    Not thoroughly bug-tested.

--- src/webapp/database-driver.js
+++ src/webapp/database-driver.js
@@ -115,7 +115,7 @@ async function getLoans(ssn) {
 
 async function transact(ssn, anum, amount, type) {
     // Ensure account num is 10 digits and amount matches money format
-    if (! /^[0-9]{10}$/.test(anum) || ! /^[0-9]+(\.[0-9]{2})?$/.test(amount))
+    if (! /^[0-9]{10}$/.test(anum) || ! /^[0-9]*(\.[0-9]{2})?$/.test(amount))
         return false;
 
     let conn;
@@ -153,7 +153,7 @@ async function transact(ssn, anum, amount, type) {
 
 async function makePayment(ssn, lnum, amount) {
     // Ensure loan num is 10 digits and amount matches money format
-    if (! /^[0-9]{10}$/.test(lnum) || ! /^[0-9]+(\.[0-9]{2})?$/.test(amount))
+    if (! /^[0-9]{10}$/.test(lnum) || ! /^[0-9]*(\.[0-9]{2})?$/.test(amount))
         return false;
 
     let conn;
@@ -216,7 +216,7 @@ async function getCustomers() {
         }
         table += "</table>";
 
-        let form = `<p>Add a customer:\n<form method="POST">\
+        let form = `<form method="POST">\
                     <label for="ssn">SSN:</label>\
                     <input type="text" id="ssn" name="ssn"><br>\
                     <label for="name">Name:</label>\
@@ -265,12 +265,339 @@ async function createCustomer(ssn, name, phone, address) {
     }
 }
 
-module.exports.doesSsnExist = doesSsnExist;
-module.exports.getBioData = getBioData;
-module.exports.getAccounts = getAccounts;
-module.exports.getLoans = getLoans;
-module.exports.makePayment = makePayment;
-module.exports.transact = transact;
-module.exports.getBankData = getBankData;
-module.exports.getCustomers = getCustomers;
-module.exports.createCustomer = createCustomer;
+async function getAllAccounts() {
+    let conn;
+    try {
+        conn = await managerPool.getConnection();
+        const rows = await conn.query("select * from (DEPOSIT natural join CUST_ACCT)");
+        if (rows.length == 0)
+            return "<p>No loans found.";
+
+        let table = "<table><tr><th>SSN</th><th>Account number</th><th>Balance</th><th>Account type</th><th>Rate class</th></tr>";
+        for (let i in rows) {
+            table += `<tr><td>${rows[i].Ssn}</td><td>${rows[i].Acct_num}</td><td>\$${rows[i].Balance}</td><td>${rows[i].Acct_type}</td><td>${rows[i].Rate}</td></tr>`;
+        }
+        table += "</table>";
+
+        let form = `<form method="POST">\
+                    <label for="ssn">SSN:</label>\
+                    <input type="text" id="ssn" name="ssn"><br>\
+                    <label for="anum">Account number:</label>\
+                    <input type="text" id="anum" name="anum"><br>\
+                    <label for="balance">Balance: $</label>\
+                    <input type="text" id="balance" name="balance"><br>\
+                    <label for="rate">Interest rate class:</label>\
+                    <input type="text" id="rate" name="rate"><br>\
+                    <label for="ltype">Account type:</label>\
+                    <input type="text" id="atype" name="atype"><br>\
+                    <input type="submit" value="Submit">\
+                    </form>`;
+
+        return [table, form];
+    } catch (e) {
+        throw e;
+    } finally {
+        if (conn) conn.end();
+    }
+}
+
+async function createAccount(ssn, anum, balance, rate, atype) {
+    if (! /^[0-9]{9}$/.test(ssn))
+        return "<p>Invalid SSN. Must be 9 digits.";
+    if (! /^[0-9]{10}$/.test(anum))
+        return "<p>Invalid account number. Must be 10 digits.";
+    if (! /^[0-9]*(\.[0-9]{2})?$/.test(balance))
+        return "<p>Invalid money format.";
+    if (! /^[a-zA-Z0-9]{1,255}$/.test(rate))
+        return "<p>Invalid rate class.";
+    if (! /^[a-zA-Z0-9 \-]{1,255}$/.test(atype))
+        return "<p>Account type may only contain alphanumerics, spaces, or hyphens.";
+
+    balance = Number(balance);
+    let conn;
+    try {
+        conn = await managerPool.getConnection();
+        let empty = await conn.query(`select Ssn from CUSTOMER where Ssn = "${ssn}"`);
+        if (empty.length == 0)
+            return "<p>Customer does not yet exist.";
+
+        empty = await conn.query(`select Rate_type from RATE where Rate_type = "${rate}"`);
+        if (empty.length == 0)
+            return "<p>No such interest rate class.";
+
+        empty = await conn.query(`select Acct_num from DEPOSIT where Acct_num = "${anum}"`);
+        if (empty.length != 0)
+            return "<p>Account number already in use.";
+
+        await conn.query(`insert into DEPOSIT set Acct_num="${anum}", Balance=${balance}, Rate="${rate}", Acct_type="${atype}", Fee=0, Deposit_type="customer"`);
+        await conn.query(`insert into CUST_ACCT set Ssn="${ssn}", Acct_num="${anum}"`);
+        return "<p>Account created successfully.";
+    } catch (e) {
+        throw e;
+    } finally {
+        if (conn) conn.end();
+    }
+}
+
+async function getAllLoans() {
+    let conn;
+    try {
+        conn = await managerPool.getConnection();
+        const rows = await conn.query("select * from (LOAN natural join CUST_LOAN)");
+        if (rows.length == 0)
+            return "<p>No loans found.";
+
+        let table = "<table><tr><th>SSN</th><th>Loan number</th><th>Balance</th><th>Next payment due</th><th>Months remaining</th><th>Rate class</th><th>Loan type</th></tr>";
+        for (let i in rows) {
+            table += `<tr><td>${rows[i].Ssn}</td><td>${rows[i].Loan_num}</td><td>\$${rows[i].Balance}</td><td>\$${rows[i].Payment_due}</td><td>${rows[i].Months_remaining}</td><td>${rows[i].Rate}</td><td>${rows[i].Loan_type}</td></tr>`;
+        }
+        table += "</table>";
+
+        let form = `<form method="POST">\
+                    <label for="ssn">SSN:</label>\
+                    <input type="text" id="ssn" name="ssn"><br>\
+                    <label for="lnum">Loan number:</label>\
+                    <input type="text" id="lnum" name="lnum"><br>\
+                    <label for="balance">Amount: $</label>\
+                    <input type="text" id="balance" name="balance"><br>\
+                    <label for="rate">Interest rate class:</label>\
+                    <input type="text" id="rate" name="rate"><br>\
+                    <input type="radio" id="fixed" name="rtype" value="fixed">\
+                    <label for="fixed">Fixed-rate</label><br>\
+                    <input type="radio" id="variable" name="rtype" value="variable">\
+                    <label for="variable">Variable</label><br>\
+                    <label for="ltype">Loan type:</label>\
+                    <input type="text" id="ltype" name="ltype"><br>\
+                    <label for="months">Months:</label>\
+                    <input type="text" id="months" name="months"><br>\
+                    <label for="rate">Payment per month: $</label>\
+                    <input type="text" id="paymentDue" name="paymentDue"><br>\
+                    <input type="submit" value="Submit">\
+                    </form>`;
+
+        return [table, form];
+    } catch (e) {
+        throw e;
+    } finally {
+        if (conn) conn.end();
+    }
+}
+
+async function createLoan(ssn, lnum, balance, rate, ltype, rtype, months, paymentDue) {
+    if (! /^[0-9]{9}$/.test(ssn))
+        return "<p>Invalid SSN. Must be 9 digits.";
+    if (! /^[0-9]{10}$/.test(lnum))
+        return "<p>Invalid loan number. Must be 10 digits.";
+    if (! /^[0-9]*(\.[0-9]{2})?$/.test(balance))
+        return "<p>Invalid money format.";
+    if (! /^[a-zA-Z0-9]{1,255}$/.test(rate))
+        return "<p>Invalid rate class.";
+    if (! /^[a-zA-Z0-9 \-]{1,255}$/.test(ltype))
+        return "<p>Account type may only contain alphanumerics, spaces, or hyphens.";
+    if (rtype != "fixed" && rtype != "variable")
+        return "<p>Quit messing with the HTML.";
+    if (! /^[0-9]+$/.test(months))
+        return "<p>Invalid months.";
+    if (! /^[0-9]*(\.[0-9]{2})?$/.test(paymentDue))
+        return "<p>Invalid money format.";
+
+    balance = Number(balance);
+    months = Number(months);
+    paymentDue = Number(paymentDue);
+    let conn;
+    try {
+        conn = await managerPool.getConnection();
+        let empty = await conn.query(`select Ssn from CUSTOMER where Ssn = "${ssn}"`);
+        if (empty.length == 0)
+            return "<p>Customer does not yet exist.";
+
+        empty = await conn.query(`select Rate_type from RATE where Rate_type = "${rate}"`);
+        if (empty.length == 0)
+            return "<p>No such interest rate class.";
+
+        empty = await conn.query(`select Loan_num from LOAN where Loan_num = "${lnum}"`);
+        if (empty.length != 0)
+            return "<p>Loan number already in use.";
+
+        await conn.query(`insert into LOAN set Loan_num="${lnum}", Balance=${balance}, Payment_due=${paymentDue}, Rate="${rate}"`);
+        await conn.query(`insert into CUST_LOAN set Loan_num="${lnum}", Ssn="${ssn}", Months_remaining=${months}, Rate_type="${rtype}", Loan_type="${ltype}", Fee=0`);
+        return "<p>Loan created successfully.";
+    } catch (e) {
+        throw e;
+    } finally {
+        if (conn) conn.end();
+    }
+}
+
+async function getFedFunds() {
+    let conn;
+    try {
+        conn = await managerPool.getConnection();
+        const lendrows = await conn.query("select Loan_num, Balance, Bank_name from (LOAN natural join FED_FUNDS_LOAN)");
+        if (lendrows.length == 0)
+            return "<p>No loans found.";
+
+        let lendtable = "<table><tr><th>Loan number</th><th>Bank</th><th>Balance</th></tr>";
+        for (let i in lendrows) {
+            lendtable += `<tr><td>${lendrows[i].Loan_num}</td><td>${lendrows[i].Bank_name}</td><td>\$${lendrows[i].Balance}</td></tr>`;
+        }
+        lendtable += "</table>";
+
+        const borrowrows = await conn.query("select Acct_num, Balance, Bank_name from DEPOSIT where Deposit_type = \"fedfunds\"");
+        if (borrowrows.length == 0)
+            return "<p>No borrows found.";
+
+        let borrowtable = "<table><tr><th>Deposit number</th><th>Bank</th><th>Balance</th></tr>";
+        for (let i in borrowrows) {
+            borrowtable += `<tr><td>${borrowrows[i].Acct_num}</td><td>${borrowrows[i].Bank_name}</td><td>\$${borrowrows[i].Balance}</td></tr>`;
+        }
+        borrowtable += "</table>";
+
+        let form = `<form method="POST">\
+                    <input type="radio" id="lend" value="lend" name="type">\
+                    <label for="lend">Lend</label><br>\
+                    <input type="radio" id="borrow" value="borrow" name="type">\
+                    <label for="borrow">Borrow</label><br>\
+                    <label for="name">Bank:</label>\
+                    <input type="text" id="name" name="name"><br>\
+                    <label for="address">Bank address:</label>\
+                    <input type="text" id="address" name="address"><br>\
+                    <label for="lnum">Account number:</label>\
+                    <input type="text" id="lnum" name="lnum"><br>\
+                    <label for="balance">Amount: $</label>\
+                    <input type="text" id="balance" name="balance"><br>\
+                    <input type="submit" value="Submit">\
+                    </form>`;
+
+        return [lendtable, borrowtable, form];
+    } catch (e) {
+        throw e;
+    } finally {
+        if (conn) conn.end();
+    }
+}
+
+async function createFedFunds(name, address, lnum, balance, type) {
+    // Check for valid fields
+    if (! /^[a-zA-Z0-9 \-\.]{1,255}$/.test(name))
+        return "<p>Invalid name. Must only contain alphanumerics, spaces, hyphens, and periods.";
+    if (! /^[a-zA-Z0-9 \-\.,]{1,255}$/.test(address))
+        return "<p>Invalid address. Must only contain alphanumerics, spaces, hyphens, commas, and periods.";
+    if (! /^[0-9]{10}$/.test(lnum))
+        return "<p>Invalid account number. Must be 10 digits.";
+    if (! /^[0-9]*(\.[0-9]{2})?$/.test(balance))
+        return "<p>Invalid money format.";
+    if (!(type == "lend") && !(type == "borrow"))
+        return "<p>Quit editing the HTML.";
+
+    balance = Number(balance);
+
+    const lend = (type == "lend")
+    let conn;
+    try {
+        conn = await managerPool.getConnection();
+        const bank = await conn.query(`select Name from EXTERN_BANK where Name = "${name}"`);
+        if (bank.length != 0)
+            return "<p>Each bank can only borrow or lend once.";
+
+        let account;
+        if (lend) {
+            account = await conn.query(`select Loan_num from LOAN where Loan_num = ${lnum}`);
+        } else {
+            account = await conn.query(`select Acct_num from DEPOSIT where Acct_num = ${lnum}`);
+        }
+        if (account.length != 0)
+            return "<p>Account number is already in use.";
+
+        await conn.query(`insert into EXTERN_BANK set Name="${name}", Address="${address}"`);
+        if (lend) {
+            await conn.query(`insert into LOAN set Loan_num="${lnum}", Balance=${balance}, Rate="FedFundsOvernight"`);
+            await conn.query(`insert into FED_FUNDS_LOAN set Loan_num="${lnum}", Bank_name="${name}"`);
+        } else {
+            await conn.query(`insert into DEPOSIT set Acct_num="${lnum}", Balance=${balance}, Rate="FedFundsOvernight", Bank_name="${name}", Deposit_type="fedfunds"`);
+        }
+        return "<p>Addition successful.";
+    } catch (e) {
+        throw e;
+    } finally {
+        if (conn) conn.end();
+    }
+}
+
+async function getRates() {
+    let conn;
+    try {
+        conn = await managerPool.getConnection();
+        const rows = await conn.query("select Rate_type, Amount*100 as Percent from RATE");
+        if (rows.length == 0)
+            return "<p>No rates found.";
+
+        let table = "<table><tr><th>Rate class</th><th>Amount</th></tr>";
+        for (let i in rows) {
+            table += `<tr><td>${rows[i].Rate_type}</td><td>${rows[i].Percent}%</td></tr>`;
+        }
+        table += "</table>";
+
+        let form = `<form method="POST">\
+                    <label for="rtype">Class:</label>\
+                    <input type="text" id="rtype" name="rtype"><br>\
+                    <label for="amount">Amount:</label>\
+                    <input type="text" id="amount" name="amount">%<br>\
+                    <input type="submit" value="Submit">
+                    </form>`;
+        return [table, form];
+    } catch (e) {
+        throw e;
+    } finally {
+        if (conn) conn.end();
+    }
+}
+
+async function modifyRate(rtype, amount) {
+    if (! /^[a-zA-Z]{1,255}$/.test(rtype))
+        return "<p>Invalid rate class.";
+    if (! /^[0-9]{0,2}(\.[0-9]{1,4})?$/.test(amount))
+        return "<p>Invalid rate.";
+
+    amount = Number(amount)/100;
+
+    let conn;
+    try {
+        conn = await managerPool.getConnection();
+        let oldRate = await conn.query(`select * from RATE where Rate_type = "${rtype}"`);
+
+        if (oldRate.length == 0)
+            return "<p>No such rate class.";
+
+        oldRate = Number(oldRate[0].Amount);
+
+        const result = await conn.query(`WITH TEMP(Balance, Percent) AS (SELECT Balance, Amount AS Percent FROM (LOAN JOIN RATE ON Rate = Rate_type) WHERE Rate_type = "${rtype}") SELECT SUM(Balance * (${amount})) - SUM(Balance * Percent) AS Profit FROM TEMP`);
+
+        await conn.query(`update RATE set Amount=${amount} where Rate_type = "${rtype}"`);
+        return `<p>Annual profit will change by \$${result[0].Profit}`;
+    } catch (e) {
+        throw e;
+    } finally {
+        if (conn) conn.end();
+    }
+}
+
+module.exports = {
+    doesSsnExist: doesSsnExist,
+    getBioData: getBioData,
+    getAccounts: getAccounts,
+    getLoans: getLoans,
+    makePayment: makePayment,
+    transact: transact,
+    getBankData: getBankData,
+    getCustomers: getCustomers,
+    createCustomer: createCustomer,
+    getAllAccounts: getAllAccounts,
+    createAccount: createAccount,
+    getAllLoans: getAllLoans,
+    createLoan: createLoan,
+    getFedFunds: getFedFunds,
+    createFedFunds: createFedFunds,
+    getRates: getRates,
+    modifyRate: modifyRate
+};
--- src/webapp/index.js
+++ src/webapp/index.js
@@ -1,15 +1,6 @@
 "use strict";
 
-const {doesSsnExist,
-       getBioData,
-       getAccounts,
-       getLoans,
-       makePayment,
-       transact,
-       getBankData,
-       getCustomers,
-       createCustomer
-} = require("./database-driver.js");
+const driver = require("./database-driver.js");
 
 const express = require("express");
 const path = require("path");
@@ -31,6 +22,10 @@ app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "html", "index.html"));
 });
 
+app.get("/stylesheet.css", function(req, res) {
+    res.sendFile(path.join(__dirname, "stylesheet.css"));
+});
+
 // If user is not logged in, show a login page.
 // Otherwise, redirect to /customer
 app.get("/login", function (req, res) {
@@ -42,7 +37,7 @@ app.get("/login", function (req, res) {
 
 // Log user in
 app.post("/login", async function (req, res) {
-    if (!(await doesSsnExist(req.body.ssn)))
+    if (!(await driver.doesSsnExist(req.body.ssn)))
         res.render("login", {failed: true});
     else {
         req.session.ssn = req.body.ssn;
@@ -57,76 +52,124 @@ app.get("/logout", function (req, res) {
 });
 
 app.get("/customer", async function (req, res) {
-    if (!req.session.ssn || !(await doesSsnExist(req.session.ssn)))
+    if (!req.session.ssn || !(await driver.doesSsnExist(req.session.ssn)))
         res.redirect("/login");
     else {
-        const biodata = await getBioData(req.session.ssn);
+        const biodata = await driver.getBioData(req.session.ssn);
         res.render('customer', {biodata: biodata});
     }
 });
 
 app.get("/customer/accounts", async function (req, res) {
-    if (!req.session.ssn || !(await doesSsnExist(req.session.ssn)))
+    if (!req.session.ssn || !(await driver.doesSsnExist(req.session.ssn)))
         res.redirect("/login");
     else {
-        const [acctTable, transacForm] = await getAccounts(req.session.ssn);
-        res.render("accounts", {acctTable: acctTable, transacForm: transacForm});
+        const [table, form] = await driver.getAccounts(req.session.ssn);
+        res.render("accounts", {table: table, form: form});
     }
 });
 
 // Make account transaction
 app.post("/customer/accounts", async function (req, res) {
-    if (!req.session.ssn || !(await doesSsnExist(req.session.ssn)))
+    if (!req.session.ssn || !(await driver.doesSsnExist(req.session.ssn)))
         res.redirect("/login");
     else {
-        const success = await transact(req.session.ssn, req.body.anum, req.body.amount, req.body.type);
-        const [acctTable, transacForm] = await getAccounts(req.session.ssn);
+        const success = await driver.transact(req.session.ssn, req.body.anum, req.body.amount, req.body.type);
+        const [table, form] = await driver.getAccounts(req.session.ssn);
         if (success)
-            res.render("accounts", {acctTable: acctTable, transacForm: transacForm, success: true});
+            res.render("accounts", {table: table, form: form, success: true});
         else
-            res.render("accounts", {acctTable: acctTable, transacForm: transacForm, failed: true});
+            res.render("accounts", {table: table, form: form, failed: true});
     }
 });
 
 app.get("/customer/loans", async function (req, res) {
-    if (!req.session.ssn || !(await doesSsnExist(req.session.ssn)))
+    if (!req.session.ssn || !(await driver.doesSsnExist(req.session.ssn)))
         res.redirect("/login");
     else {
-        const [loanTable, paymentForm] = await getLoans(req.session.ssn);
-        res.render("loans", {loanTable: loanTable, paymentForm: paymentForm});
+        const [table, form] = await driver.getLoans(req.session.ssn);
+        res.render("loans", {table: table, form: form});
     }
 });
 
 // Make loan payment
 app.post("/customer/loans", async function (req, res) {
-    if (!req.session.ssn || !(await doesSsnExist(req.session.ssn)))
+    if (!req.session.ssn || !(await driver.doesSsnExist(req.session.ssn)))
         res.redirect("/login");
     else {
-        const success = await makePayment(req.session.ssn, req.body.lnum, req.body.amount);
-        const [loanTable, paymentForm] = await getLoans(req.session.ssn);
+        const success = await driver.makePayment(req.session.ssn, req.body.lnum, req.body.amount);
+        const [table, form] = await driver.getLoans(req.session.ssn);
         if (success)
-            res.render("loans", {loanTable: loanTable, paymentForm: paymentForm, success: true});
+            res.render("loans", {table: table, form: form, success: true});
         else
-            res.render("loans", {loanTable: loanTable, paymentForm: paymentForm, failed: true});
+            res.render("loans", {table: table, form: form, failed: true});
     }
 });
 
 
 app.get("/manager", async function (req, res) {
-    const bankdata = await getBankData();
+    const bankdata = await driver.getBankData();
     res.render("manager", {bankdata: bankdata});
 });
 
 app.get("/manager/customers", async function (req, res) {
-    const [custTable, custForm] = await getCustomers();
-    res.render("allcustomers", {custTable: custTable, custForm: custForm});
+    const [table, form] = await driver.getCustomers();
+    res.render("allcustomers", {table: table, form: form});
 });
 
 // Create a customer
 app.post("/manager/customers", async function (req, res) {
-    const results = await createCustomer(req.body.ssn, req.body.name, req.body.phone, req.body.address);
-    const [custTable, custForm] = await getCustomers();
-    res.render("allcustomers", {custTable: custTable, custForm: custForm, results: results});
+    const results = await driver.createCustomer(req.body.ssn, req.body.name, req.body.phone, req.body.address);
+    const [table, form] = await driver.getCustomers();
+    res.render("allcustomers", {table: table, form: form, results: results});
+});
+
+app.get("/manager/accounts", async function (req, res) {
+    const [table, form] = await driver.getAllAccounts();
+    res.render("allaccounts", {table: table, form: form});
+});
+
+// Create an account
+app.post("/manager/accounts", async function (req, res) {
+    const results = await driver.createAccount(req.body.ssn, req.body.anum, req.body.balance, req.body.rate, req.body.atype);
+    const [table, form] = await driver.getAllAccounts();
+    res.render("allaccounts", {table: table, form: form, results: results});
+});
+
+app.get("/manager/loans", async function (req, res) {
+    const [table, form] = await driver.getAllLoans();
+    res.render("allloans", {table: table, form: form});
+});
+
+// Create a loan
+app.post("/manager/loans", async function (req, res) {
+    const results = await driver.createLoan(req.body.ssn, req.body.lnum, req.body.balance, req.body.rate, req.body.ltype, req.body.rtype, req.body.months, req.body.paymentDue);
+    const [table, form] = await driver.getAllLoans();
+    res.render("allloans", {table: table, form: form, results: results});
+});
+
+app.get("/manager/fedfunds", async function (req, res) {
+    const [lendtable, borrowtable, form] = await driver.getFedFunds();
+    res.render("fedfunds", {lendtable: lendtable, borrowtable: borrowtable, form: form});
+});
+
+// Add a federal fund loan
+app.post("/manager/fedfunds", async function (req, res) {
+    const results = await driver.createFedFunds(req.body.name, req.body.address, req.body.lnum, req.body.balance, req.body.type);
+    const [lendtable, borrowtable, form] = await driver.getFedFunds();
+    res.render("fedfunds", {lendtable: lendtable, borrowtable: borrowtable, form: form, results: results});
+});
+
+app.get("/manager/rates", async function (req, res) {
+    const [table, form] = await driver.getRates();
+    res.render("rates", {table: table, form: form});
+});
+
+// Alter an interest rate
+app.post("/manager/rates", async function (req, res) {
+    const results = await driver.modifyRate(req.body.rtype, req.body.amount);
+    const [table, form] = await driver.getRates();
+    res.render("rates", {table: table, form: form, results: results});
 });
 
 app.listen(port, function(){
